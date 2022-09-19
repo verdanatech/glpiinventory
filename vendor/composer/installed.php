f83b63cc95c8345724ec4e6251144504df91cec2
Márcio Fernandes <marcio.fernandes@verdanatech.com>
2022-09-19 16:19:23 -0300

    GLPI inventory GLPI 10

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => '1.0.4',
+        'version' => '1.0.4.0',
+        'reference' => '1c2c084f5edb620804c2b2b4f9a3fc3b6cb75c7e',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => '1.0.4',
+            'version' => '1.0.4.0',
+            'reference' => '1c2c084f5edb620804c2b2b4f9a3fc3b6cb75c7e',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);

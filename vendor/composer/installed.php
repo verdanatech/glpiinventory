87152bb91f6e5fa82a77c06d1b760926771a7494
verdanatech-emanoel <emanoel.dias@verdanatech.com>
2026-06-26 09:00:07 -0300

    Add New Version

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => '__root__',
-        'pretty_version' => '1.5.7',
-        'version' => '1.5.7.0',
-        'reference' => 'acf9a35b6f4a0d095bc6dc96896275768c593fc8',
+        'pretty_version' => '1.5.8',
+        'version' => '1.5.8.0',
+        'reference' => 'f0d46bb9a2b12f9c9773d8bb5b62e68be268e0bc',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         '__root__' => array(
-            'pretty_version' => '1.5.7',
-            'version' => '1.5.7.0',
-            'reference' => 'acf9a35b6f4a0d095bc6dc96896275768c593fc8',
+            'pretty_version' => '1.5.8',
+            'version' => '1.5.8.0',
+            'reference' => 'f0d46bb9a2b12f9c9773d8bb5b62e68be268e0bc',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
